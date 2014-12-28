0afb9a06e586027a7a2e227b01f3f83f2c4cf133
David Barton <dave@oneit.com.au>
2014-12-28 18:02:26 +0800

    Additional unit tests and modularisation of unit testing

--- tests/test.sh
+++ tests/test.sh
@@ -8,12 +8,51 @@
 
 TMPDIR=/tmp/zbackup-tar/
 TESTDATA=$1
+TODO_BUG=1
 
-rm -rf $TMPDIR
-mkdir -pv $TMPDIR/zbackup $TMPDIR/restored
 
-zbackup init --non-encrypted $TMPDIR/zbackup/
-find $TESTDATA -name "*.txt" -print0 | xargs -0 rm -v
+logResult()
+{
+    echo "[UNIT TEST] $1"
+}
+
+
+logFailResult()
+{
+    echo "[UNIT TEST] $1"  1>&2
+}
+
+
+checkForSuccess ()
+{
+   if [ "$?" -eq 0 ]; then
+        logResult "$1"
+    else
+        logFailResult "$2"
+
+        if [ -z "$3" ]; then
+            exit 1
+        else
+            logFailResult "KNOWN BUG - Supressing Failure"
+        fi
+    fi
+}
+
+
+checkForFailure ()
+{
+   if [ "$?" -eq 0 ]; then
+        logFailResult "$1"
+
+        if [ -z "$3" ]; then
+            exit 1
+        else
+            logFailResult "KNOWN BUG - Supressing Failure"
+        fi
+    else
+        logResult "$2"
+    fi
+}
 
 
 function backupAndRestoreDir ()
@@ -28,7 +67,7 @@ function backupAndRestoreDir ()
     fi
     
     echo PREVBACKUP $PREVBACKUP NEWBACKUP $TMPDIR/zbackup/backups/$BACKUPNAME
-    zbackup-tar create --previousBackup "$PREVBACKUP" --newBackup $TMPDIR/zbackup/backups/$BACKUPNAME --maxAge 0.05 --maxAgeJitter 0.03 $TESTDATA/
+    zbackup-tar create --previousBackup "$PREVBACKUP" --newBackup $TMPDIR/zbackup/backups/$BACKUPNAME --maxAge 0.03 --maxAgeJitter 0.02 $TESTDATA/
 
     echo Restore $BACKUPNAME
 
@@ -40,160 +79,373 @@ function backupAndRestoreDir ()
     echo Checking $BACKUPNAME
 
     diff -rq $TESTDATA/ $TMPDIR/restored/
-
-    if [ "$?" -eq 0 ]; then
-        echo SUCCESS $BACKUPNAME is the same
-    else
-        echo FAIL Restoring $BACKUPNAME
-        exit 1
-    fi
+    checkForSuccess "SUCCESS $BACKUPNAME is the same" "FAIL Restoring $BACKUPNAME"
 
     zbackup restore $TMPDIR/zbackup/backups/$BACKUPNAME.manifest > /tmp/$BACKUPNAME.manifest
 }
 
 
-function sleepAvoidCollision ()
+function longSleep ()
 {
-    echo "Sleeping for 10 seconds so we don't get date collisions"
-    sleep 10 
+    SLEEP_PERIOD=$1
+    SLEEP_MESSAGE=$2
+
+    echo -n "Sleeping for $SLEEP_PERIOD $SLEEP_MESSAGE"
+
+    for i in `seq 1 $SLEEP_PERIOD`; do
+        sleep 1
+
+        if [ $((i % 5)) -eq 0 ]; then
+            echo -n $i
+        else
+            echo -n .
+        fi
+    done;
+
+    echo DONE
 }
 
 
 
-echo "######## Backup 1 - Initial Backup ########"
-backupAndRestoreDir "" backup01.tar
-sleepAvoidCollision
+function sleepAvoidCollision ()
+{
+    SLEEP_PERIOD=5
+
+    echo -n "Sleeping for $SLEEP_PERIOD seconds so we don't get date collisions "
+    for i in `seq 1 $SLEEP_PERIOD`; do
+        sleep 1
+        echo -n .
+    done;
+
+    echo DONE
+}
+
+
+function test1 ()
+{
+    logResult "######## Backup 1 - Initial Backup ########"
+    backupAndRestoreDir "" backup01.tar
+    sleepAvoidCollision
+}
+
+
+function test2 ()
+{
+    logResult "######## Backup 2 - No Changes ########"
+    backupAndRestoreDir backup01.tar backup02.tar
+
+    diff /tmp/backup01.tar.manifest /tmp/backup02.tar.manifest
+
+    checkForSuccess "SUCCESS Backup manifest 1 and 2 are identical" "FAIL manifest 1 and 2 are different"
+
+    sleepAvoidCollision
+}
 
 
 
-"######## Backup 2 - No Changes ########"
-backupAndRestoreDir backup01.tar backup02.tar
+function test3 ()
+{
+    logResult "######## Backup 3 - A New File ########"
+    date > $TESTDATA/file.txt
 
-diff /tmp/backup01.tar.manifest /tmp/backup02.tar.manifest
-
-if [ "$?" -eq 0 ]; then
-    echo SUCCESS Backup manifest 1 and 2 are identical
-else
-    echo FAIL Restoring backup 1
-    return
-fi
-
-sleepAvoidCollision
+    backupAndRestoreDir backup02.tar backup03.tar
+    sleepAvoidCollision
+}
 
 
+function test4 ()
+{
+    logResult "######## Backup 4 - Changed Files ########"
+    date > $TESTDATA/file.txt
+    date > $TESTDATA/folder2/file.txt
 
-
-echo "######## Backup 3 - A New File ########"
-date > $TESTDATA/file.txt
-
-backupAndRestoreDir backup02.tar backup03.tar
-sleepAvoidCollision
+    backupAndRestoreDir backup03.tar backup04.tar
+    sleepAvoidCollision
+}
 
 
 
-echo "######## Backup 4 - Changed Files ########"
-date > $TESTDATA/file.txt
-date > $TESTDATA/folder2/file.txt
+function test5 ()
+{
+    logResult "######## Backup 5 - Removed Files ########"
+    find $TESTDATA -name "*.txt" -print0 | xargs -0 rm -v
 
-backupAndRestoreDir backup03.tar backup04.tar
-sleepAvoidCollision
+    backupAndRestoreDir backup04.tar backup05.tar
+    sleepAvoidCollision
+}
 
 
 
-echo "######## Backup 5 - Removed Files ########"
+function test6 ()
+{
+    logResult "######## Test 6 - Moving the backup ########"
+    export BACKUP=06
+    backupAndRestoreDir backup05.tar backup$BACKUP.tar
+
+    mkdir -pv $TMPDIR/foo/bar/
+    mv $TMPDIR/zbackup $TMPDIR/foo/bar/
+
+    echo Restore backup $BACKUP
+    cd $TMPDIR/restored/
+    zbackup-tar restore --backup $TMPDIR/foo/bar/zbackup/backups/backup$BACKUP.tar
+
+    echo Checking backup $BACKUP
+    diff -rq $TESTDATA/ $TMPDIR/restored/
+
+    checkForSuccess "SUCCESS $BACKUP is the same" "FAIL $BACKUP is differente"
+
+    mv $TMPDIR/foo/bar/zbackup $TMPDIR/
+
+    sleepAvoidCollision
+}
+
+
+
+function test7 ()
+{
+    logResult "######## Test 7 - Handling errors ########"
+    export BACKUP=07
+    backupAndRestoreDir backup06.tar backup$BACKUP.tar
+
+    chmod gou-rwx /tmp/zbackup-tar/zbackup/index/
+
+    echo Restore backup $BACKUP AFTER remiving permissions
+    cd $TMPDIR/restored/
+    zbackup-tar restore --backup $TMPDIR/zbackup/backups/backup$BACKUP.tar
+
+    checkForFailure "FAIL - backup should have FAILED $BACKUP" "SUCCESS Restoring the backup returned a non-0 error code"
+
+    chmod --reference=/tmp/zbackup-tar/zbackup/bundles /tmp/zbackup-tar/zbackup/index/
+
+    sleepAvoidCollision
+}
+
+
+
+
+function test8 ()
+{
+    logResult "######## Backup 8 - Add txt files ########"
+    date > $TESTDATA/file.txt
+    date > $TESTDATA/folder2/file.txt
+
+    backupAndRestoreDir backup07.tar backup08.tar
+    sleepAvoidCollision
+}
+
+
+
+function test9 ()
+{
+    logResult "######## Backup 9 - Exclude txt files ########"
+    export BACKUP=09
+    rm -rf $TMPDIR/restored/*
+    echo Initial backup $BACKUP
+    zbackup-tar create --previousBackup $TMPDIR/zbackup/backups/backup08.tar --newBackup $TMPDIR/zbackup/backups/backup$BACKUP.tar --maxAge 0.05 --maxAgeJitter 0.03 --exclude "*.txt" $TESTDATA/
+
+
+    echo Restore backup $BACKUP
+    cd $TMPDIR/restored/
+    zbackup-tar restore --backup $TMPDIR/zbackup/backups/backup$BACKUP.tar
+
+    zbackup restore $TMPDIR/zbackup/backups/backup$BACKUP.tar.manifest > /tmp/backup$BACKUP.tar.manifest
+
+    echo Checking backup $BACKUP
+    diff -rq $TESTDATA/ $TMPDIR/restored/
+
+    checkForFailure "FAIL txt files should be different" "SUCCESS backup $BACKUP is different"
+
+    find $TESTDATA/ -name "*.txt" -print0 | xargs -0 rm -v
+
+    diff -rq $TESTDATA/ $TMPDIR/restored/
+
+    checkForSuccess "SUCCESS After removing txt files, backup should be the same" "FAIL backup files were different" $TODO_BUG
+
+    sleepAvoidCollision
+}
+
+
+
+function test10 ()
+{
+    logResult "######## Backup 10 - Exclude subfolder1/ files ########"
+    export BACKUP=10
+    rm -rf $TMPDIR/restored/*
+    echo Initial backup $BACKUP
+    zbackup-tar create --previousBackup $TMPDIR/zbackup/backups/backup09.tar --newBackup $TMPDIR/zbackup/backups/backup$BACKUP.tar --maxAge 0.05 --maxAgeJitter 0.03 --exclude "subfolder1/" $TESTDATA/
+
+
+    echo Restore backup $BACKUP
+    cd $TMPDIR/restored/
+    zbackup-tar restore --backup $TMPDIR/zbackup/backups/backup$BACKUP.tar
+
+    zbackup restore $TMPDIR/zbackup/backups/backup$BACKUP.tar.manifest > /tmp/backup$BACKUP.tar.manifest
+
+    echo Checking backup $BACKUP
+    diff -rq $TESTDATA/ $TMPDIR/restored/ | tee /tmp/backup$BACKUP.diff
+
+    checkForFailure "FAIL txt files should be different" "SUCCESS backup $BACKUP is different" $TODO_BUG
+
+    grep -v subfolder1/ /tmp/backup$BACKUP.diff
+
+    checkForFailure "FAIL There should be no lines not matching subfolder1/" "SUCCESS backup is the same after excluding subfolder1/" $TODO_BUG
+
+    sleepAvoidCollision
+}
+
+
+
+function test11 ()
+{
+    logResult "######## Test 11 - Handling errors backing up ########"
+    export BACKUP=11
+    chmod gou-rwx /tmp/zbackup-tar/zbackup/index/
+
+    zbackup-tar create --previousBackup $TMPDIR/zbackup/backups/backup10.tar --newBackup $TMPDIR/zbackup/backups/backup$BACKUP.tar --maxAge 0.03 --maxAgeJitter 0.02 $TESTDATA/
+
+    checkForFailure "FAIL - backup should have FAILED $BACKUP" "SUCCESS Storing the backup returned a non-0 error code"
+
+    chmod --reference=/tmp/zbackup-tar/zbackup/bundles /tmp/zbackup-tar/zbackup/index/
+
+    zbackup-tar create --previousBackup $TMPDIR/zbackup/backups/backup10.tar --newBackup $TMPDIR/zbackup/backups/backup$BACKUP.tar --maxAge 0.03 --maxAgeJitter 0.02 $TESTDATA/
+
+    checkForSuccess "SUCCESS - backup should have SUCCEEDED $BACKUP" "FAIL Backing up returned a non-0 error code"
+
+    sleepAvoidCollision
+}
+
+
+
+function test12 ()
+{
+    logResult "######## Test 12 - Ensuring backup output ########"
+    export BACKUP=12
+
+    date > $TESTDATA/file.txt
+    date > $TESTDATA/folder2/file.txt
+    touch $TESTDATA/document.pdf 
+
+    # Extend maxAge so we don't freshen any files
+    zbackup-tar create --previousBackup $TMPDIR/zbackup/backups/backup11.tar --newBackup $TMPDIR/zbackup/backups/backup$BACKUP.tar --maxAge 10 --maxAgeJitter 0.02 $TESTDATA/ > /tmp/backup$BACKUP.stdout
+
+    diff -w /tmp/backup$BACKUP.stdout $TESTDATA/../results/backup$BACKUP.stdout
+
+    checkForSuccess "SUCCESS - Output should be the same $BACKUP" "FAILURE Output is not the same as the sample" $TODO_BUG
+
+    sleepAvoidCollision
+}
+
+
+
+# Expected output is:
+# fileX     EXTRACTED from backups/blah.tar
+# fileY     EXTRACTED from backups/blahX.tar
+# where backupNN.tar is the tar file where the file resides.
+# The files are listed on the order they are extracted, so they should be listed clustered by tarfile and then in the order they were tarred
+function test13 ()
+{
+    logResult "######## Test 13 - Ensuring restore output ########"
+    export BACKUP=13
+
+    backupAndRestoreDir backup12.tar backup13.tar
+
+    cd $TMPDIR/restored/
+    rm -rf $TMPDIR/restored/*
+    zbackup-tar restore --backup $TMPDIR/zbackup/backups/backup$BACKUP.tar > /tmp/backup$BACKUP.stdout
+
+    # Sort the output of the command by filename, since the output is NOT sorted
+    sort /tmp/backup$BACKUP.stdout | sed -e "s/backup[0-9][0-9].tar/backupNN.tar/g" > /tmp/backup$BACKUP.stdout.massaged
+    diff -w /tmp/backup$BACKUP.stdout $TESTDATA/../results/backup$BACKUP.stdout
+
+    checkForSuccess "SUCCESS - Output should be the same $BACKUP" "FAILURE Output is not the same as the sample" $TODO_BUG
+
+    sleepAvoidCollision
+}
+
+
+function test14 ()
+{
+    logResult "######## Test 14 - Partial restores ########"
+    export BACKUP=14
+
+    backupAndRestoreDir backup13.tar backup14.tar
+
+    cd $TMPDIR/restored/
+    rm -rf $TMPDIR/restored/*
+    zbackup-tar restore --backup $TMPDIR/zbackup/backups/backup$BACKUP.tar folder1/
+
+    diff -rq $TESTDATA/folder1/ $TMPDIR/restored/folder1/
+
+    checkForSuccess "SUCCESS - folder1 is the same" "FAILURE folder1 is different"
+
+    if [ -d "$TMPDIR/restored/folder2" ]; then
+        logFailResult "FAIL folder2 should not have been restored"
+        exit 1
+    else
+        logResult "SUCCESS folder2 was not restored"
+    fi
+
+    sleepAvoidCollision
+}
+
+
+
+
+
+function test15 ()
+{
+    logResult "######## Backup 15 - Sleep for 2 mins ########"
+
+    longSleep 60 "To freshen files"
+
+    backupAndRestoreDir backup14.tar backup15.tar
+
+    diff /tmp/backup14.tar.manifest /tmp/backup15.tar.manifest
+
+    checkForFailure "FAIL Manifests are identical, should have been reloaded" "SUCCESS Manifest has changed"
+}
+
+
+
+function test16 ()
+{
+    logResult "######## Backup 16 - Sleep for 1 mins ########"
+
+    longSleep 45 "To freshen files"
+
+    backupAndRestoreDir backup15.tar backup16.tar
+
+    diff /tmp/backup15.tar.manifest /tmp/backup16.tar.manifest
+
+    checkForFailure "FAIL Manifests are identical, should have been reloaded" "SUCCESS Manifest has changed"
+
+    sleepAvoidCollision
+}
+
+
+
 find $TESTDATA -name "*.txt" -print0 | xargs -0 rm -v
+chmod --reference=/tmp/zbackup-tar/zbackup/bundles /tmp/zbackup-tar/zbackup/index/
+rm -rf $TMPDIR
+mkdir -pv $TMPDIR/zbackup $TMPDIR/restored
 
-backupAndRestoreDir backup04.tar backup05.tar
-sleepAvoidCollision
-
-
-
-
-echo "######## Backup 6 - Sleep for 2 mins ########"
-
-echo "Sleeping for 2 minutes to freshen some files (0.05h = 3 mins +/- 1.8 mins i.e. 1.2 - 4.8 mins"
-sleep 120 
-
-backupAndRestoreDir backup05.tar backup06.tar
-
-diff /tmp/backup05.tar.manifest /tmp/backup06.tar.manifest
-
-if [ "$?" -eq 0 ]; then
-    echo FAIL Manifests are identical, should have been reloaded
-    return
-else
-    echo SUCCESS Manifest has changed
-fi
+zbackup init --non-encrypted $TMPDIR/zbackup/
 
 
+test1
+test2
+test3
+test4
+test5
+test6
+test7
+test8
+test9
+test10
+test11
+test12
+test13
+test14
+test15
 
-echo "######## Backup 7 - Sleep for 1 mins ########"
-
-echo "Sleeping for 1 minutes to freshen some files"
-sleep 60 
-
-backupAndRestoreDir backup06.tar backup07.tar
-
-diff /tmp/backup06.tar.manifest /tmp/backup07.tar.manifest
-
-if [ "$?" -eq 0 ]; then
-    echo FAIL Manifests are identical, should have been reloaded
-    return
-else
-    echo SUCCESS Manifest has changed
-fi
-
-sleepAvoidCollision
-
-
-
-echo "######## Backup 8 - Add txt files ########"
-date > $TESTDATA/file.txt
-date > $TESTDATA/folder2/file.txt
-
-backupAndRestoreDir backup07.tar backup08.tar
-sleepAvoidCollision
-
-
-
-echo "######## Backup 9 - Exclude txt files ########"
-rm -rf $TMPDIR/restored/*
-echo Initial backup 9
-zbackup-tar create --previousBackup $TMPDIR/zbackup/backups/backup08.tar --newBackup $TMPDIR/zbackup/backups/backup09.tar --maxAge 0.05 --maxAgeJitter 0.03 --exclude "*.txt" $TESTDATA/
-
-
-echo Restore backup 9
-cd $TMPDIR/restored/
-zbackup-tar restore --backup $TMPDIR/zbackup/backups/backup09.tar
-
-zbackup restore $TMPDIR/zbackup/backups/backup09.tar.manifest > /tmp/backup09.tar.manifest
-
-echo Checking backup 9
-diff -rq $TESTDATA/ $TMPDIR/restored/
-
-if [ "$?" -eq 0 ]; then
-    echo FAIL txt files should be different
-    return
-else
-    echo SUCCESS backup 9 is different
-fi
-
-
-find $TESTDATA/ -name "*.txt" -print0 | xargs -0 rm -v
-
-diff -rq $TESTDATA/ $TMPDIR/restored/
-
-if [ "$?" -eq 0 ]; then
-    echo SUCCESS After removing txt files, backup should be the same
-else
-    echo FAIL backup files were different
-    return
-fi
-
-
-echo "Sleeping for 10 seconds so we don't get date collisions"
-sleep 10 
-
-
-
-
+find $TESTDATA -name "*.txt" -print0 | xargs -0 rm -v
